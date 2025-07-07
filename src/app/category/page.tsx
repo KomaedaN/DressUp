0f971e02e8b358e9d401cb98552c4688ed38f301
KomaedaN <thomas@DESKTOP-DF75MQD>
2025-07-07 02:00:31 +0200

    update suspense

--- src/app/category/page.tsx
+++ src/app/category/page.tsx
@@ -1,45 +1,17 @@
 import ProductCard from "@/components/productCard";
+import { Suspense } from "react";
 
-const produits = [
-  { nom: "T-shirt", categorieId: 1, prix: 150, image: "/clothes.gif" },
-  { nom: "T-shirt", categorieId: 2, prix: 150, image: "/clothes.gif" },
-  { nom: "T-shirt", categorieId: 3, prix: 150, image: "/clothes.gif" },
-  { nom: "T-shirt", categorieId: 1, prix: 150, image: "/clothes.gif" },
-  { nom: "T-shirt", categorieId: 2, prix: 150, image: "/clothes.gif" },
-  { nom: "T-shirt", categorieId: 3, prix: 150, image: "/clothes.gif" },
-  { nom: "T-shirt", categorieId: 1, prix: 150, image: "/clothes.gif" },
-  { nom: "T-shirt", categorieId: 2, prix: 150, image: "/clothes.gif" },
-  { nom: "T-shirt", categorieId: 3, prix: 150, image: "/clothes.gif" },
-  { nom: "short", categorieId: 1, prix: 200, image: "/clothes.gif" },
-  { nom: "pull", categorieId: 1, prix: 100, image: "/clothes.gif" },
-];
-
-export default async function CategoryPage({
-  searchParams,
-}: {
-  searchParams: Record<string, string | string[] | undefined>;
-}) {
-  const currentCategoryId = searchParams.cat;
-
-  const products = produits.filter(
-    (prod) => String(prod.categorieId) === currentCategoryId
-  );
-
+export default async function CategoryPage() {
   return (
     <section className="h-[calc(100vh-100px)]">
       <nav className="h-[50px] bg-gray-500 flex pl-[150px] pr-[150px] items-center text-white">
-        <p>Catégorie {currentCategoryId}</p>
+        <p>Catégorie </p>
       </nav>
       <div className="grid grid-cols-[6fr_1fr] pl-[150px] pr-[150px]">
         <section className="grid grid-cols-3 justify-items-center gap-6">
-          {products.map((product, idx) => (
-            <ProductCard
-              key={product.nom + idx}
-              nom={product.nom}
-              prix={product.prix}
-              image={product.image}
-            />
-          ))}
+          <Suspense>
+            <ProductCard />
+          </Suspense>
         </section>
         <div className="h-[calc(100vh-150px)] w-[25rem] bg-red-500"></div>
       </div>
--- src/components/productCard.tsx
+++ src/components/productCard.tsx
@@ -1,23 +1,51 @@
+"use client";
+import { useSearchParams } from "next/navigation";
 import Image from "next/image";
 
 export type ProductProps = {
   nom: string;
+  categorieId?: number;
   prix?: number;
   image?: string;
 };
 
-export default function ProductCard({ nom, prix, image }: ProductProps) {
+const produits: ProductProps[] = [
+  { nom: "T-shirt", categorieId: 1, prix: 150, image: "/clothes.gif" },
+  { nom: "T-shirt", categorieId: 2, prix: 150, image: "/clothes.gif" },
+  { nom: "T-shirt", categorieId: 3, prix: 150, image: "/clothes.gif" },
+  { nom: "T-shirt", categorieId: 1, prix: 150, image: "/clothes.gif" },
+  { nom: "T-shirt", categorieId: 2, prix: 150, image: "/clothes.gif" },
+  { nom: "T-shirt", categorieId: 3, prix: 150, image: "/clothes.gif" },
+  { nom: "T-shirt", categorieId: 1, prix: 150, image: "/clothes.gif" },
+  { nom: "T-shirt", categorieId: 2, prix: 150, image: "/clothes.gif" },
+  { nom: "T-shirt", categorieId: 3, prix: 150, image: "/clothes.gif" },
+  { nom: "short", categorieId: 1, prix: 200, image: "/clothes.gif" },
+  { nom: "pull", categorieId: 1, prix: 100, image: "/clothes.gif" },
+];
+
+export default function ProductCard() {
+  const searchParams = useSearchParams();
+  const currentCategoryId = searchParams.get("cat");
+
+  const products = produits.filter(
+    (prod) => String(prod.categorieId) === currentCategoryId
+  );
+
   return (
-    <div className="mt-8">
-      <Image
-        className="h-[32rem] w-[27rem] bg-gray-500"
-        src={image ?? "/clothes.gif"}
-        alt={nom}
-        width={100}
-        height={100}
-      />
-      <p className="mt-4">{nom}</p>
-      <p className="mt-2">{prix} €</p>
-    </div>
+    <>
+      {products.map((product, idx) => (
+        <div className="mt-8" key={idx}>
+          <Image
+            className="h-[32rem] w-[27rem] bg-gray-500"
+            src={product.image ?? "/clothes.gif"}
+            alt={product.nom}
+            width={100}
+            height={100}
+          />
+          <p className="mt-4">{product.nom}</p>
+          <p className="mt-2">{product.prix} €</p>
+        </div>
+      ))}
+    </>
   );
 }
